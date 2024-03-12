9a48db97687e5855feb451bc2918f0dbb2b5f002
ta4toDev <irina.jaudszims@gmail.com>
2024-03-12 14:18:08 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Datentypen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarieren und initialisieren der Variable wert1
+            int wert1 = 25;
+            //Deklarieren
+            int wert2;
+            //Initialisieren
+            wert2 = 15;
+            int summe = wert1 + wert2;
+            //Deklarieren mehrere Variablen in einer Zeile
+            int wert3, wert4, wert5;
+            //Verwendung von Gleitkommazahlen mit Double
+            double d1 = 333.5;
+            double d2 = 1.337;
+            double ergebnis = d1 / d2;
+
+            //Verwendung von Gleitkommazahlen mit Float
+            float f1 = 1.337f;
+
+
+            Console.WriteLine("Ergebnis " + ergebnis);
+            Console.ReadKey();
+        }
+    }
+}
